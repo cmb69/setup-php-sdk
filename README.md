52470d5e50963e4e3dd1ee3e9a207558046fe2f8
Christoph M. Becker <cmbecker69@gmx.de>
2021-06-20 18:34:59 +0200

    Minimal useful action

---
+++ README.md
@@ -0,0 +1,37 @@
+# Setup PHP-SDK Action
+
+[Github Action](https://github.com/features/actions) to set up an environment
+for building and testing PHP extensions on Windows.
+
+## Example Usage
+
+````.yml
+- id: setup-php-sdk
+  uses: cmb69/setup-php-sdk@v0.1
+  with:
+    version: 8.0
+    arch: x64
+    ts: nts
+- uses: ilammy/msvc-dev-cmd@v1
+  with:
+    arch: x64
+    toolset: ${{steps.setup-php-sdk.outputs.toolset}}
+- run: phpize
+- run: configure --enable-dbase --with-prefix=${{steps.setup-php-sdk.outputs.prefix}}
+- run: nmake
+- run: nmake test TESTS=tests
+````
+
+## Inputs
+
+- `version`: the PHP version to build for
+  (`7.0`, `7.1`, `7.2`, `7.3`, `7.4`, `8.0` or `8.1`)
+- `arch`: the architecture to build for (`x64` or `x86`)
+- `ts`: thread-safety (`nts` or `ts`)
+
+## Outputs
+
+- `toolset`: the required toolset version;
+  needs to be passed to the ilammy/msvc-dev-cmd action
+- `prefix`: the prefix of the PHP installation;
+  needs to be passed to configure
---
+++ run.ps1
@@ -0,0 +1,93 @@
+param (
+    [Parameter(Mandatory)] [String] $version,
+    [Parameter(Mandatory)] [String] $arch,
+    [Parameter(Mandatory)] [String] $ts
+)
+
+$ErrorActionPreference = "Stop"
+
+$versions = @{
+    "7.0" = "vc14"
+    "7.1" = "vc14"
+    "7.2" = "vc15"
+    "7.3" = "vc15"
+    "7.4" = "vc15"
+    "8.0" = "vs16"
+    "8.1" = "vs16"
+}
+$vs = $versions.$version
+if (-not $vs) {
+    throw "unsupported version"
+}
+
+$toolsets = @{
+    "vc14" = "14.0"
+}
+$dir = vswhere -latest -find "VC\Tools\MSVC"
+foreach ($ts in (Get-ChildItem $dir)) {
+    $tsv = "$ts".split(".")
+    if ((14 -eq $tsv[0]) -and (9 -ge $tsv[1])) {
+        $toolsets."vc14" = $ts
+    } elseif ((14 -eq $tsv[0]) -and (19 -ge $tsv[1])) {
+        $toolsets."vc15" = $ts
+    } elseif (14 -eq $tsv[0]) {
+        $toolsets."vs16" = $ts
+    }
+}
+$toolset = $toolsets.$vs
+if (-not $toolset) {
+    throw "toolset not available"
+}
+
+$temp = New-TemporaryFile | Rename-Item -NewName {$_.Name + ".zip"} -PassThru
+$url = "https://github.com/cmb69/php-sdk-binary-tools/archive/refs/heads/master.zip"
+Invoke-WebRequest $url -OutFile $temp
+Expand-Archive $temp -DestinationPath "."
+Rename-Item "php-sdk-binary-tools-master" "php-sdk"
+
+$baseurl = "https://windows.php.net/downloads/releases/archives"
+$releases = @{
+    "7.0" = "7.0.33"
+    "7.1" = "7.1.33"
+    "7.2" = "7.2.34"
+}
+$phpversion = $releases.$version
+if (-not $phpversion) {
+    $baseurl = "https://windows.php.net/downloads/releases"
+    $url = "$baseurl/releases.json"
+    $releases = Invoke-WebRequest $url | ConvertFrom-Json
+    $phpversion = $releases.$version.version
+    if (-not $phpversion) {
+        $baseurl = "https://windows.php.net/downloads/qa"
+        $url = "$baseurl/releases.json"
+        $releases = Invoke-WebRequest $url | ConvertFrom-Json
+        $phpversion = $releases.$version.version
+        if (-not $phpversion) {
+            throw "unknown version"
+        }
+    }
+}
+$version = $phpversion
+
+$tspart = if ($ts -eq "nts") {"nts-Win32"} else {"Win32"}
+
+$temp = New-TemporaryFile | Rename-Item -NewName {$_.Name + ".zip"} -PassThru
+$fname = "php-$version-$tspart-$vs-$arch.zip"
+$url = "$baseurl/$fname"
+Invoke-WebRequest $url -OutFile $temp
+Expand-Archive $temp "php-bin"
+
+$temp = New-TemporaryFile | Rename-Item -NewName {$_.Name + ".zip"} -PassThru
+$fname = "php-devel-pack-$version-$tspart-$vs-$arch.zip"
+$url = "$baseurl/$fname"
+Invoke-WebRequest $url -OutFile $temp
+Expand-Archive $temp "."
+Rename-Item "php-$version-devel-$vs-$arch" "php-dev"
+
+Add-Content $Env:GITHUB_PATH "$pwd\php-sdk\bin"
+Add-Content $Env:GITHUB_PATH "$pwd\php-sdk\msys2\usr\bin"
+Add-Content $Env:GITHUB_PATH "$pwd\php-bin"
+Add-Content $Env:GITHUB_PATH "$pwd\php-dev"
+
+Write-Output "::set-output name=toolset::$toolset"
+Write-Output "::set-output name=prefix::$pwd\php-bin"
