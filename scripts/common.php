110e7b050a753827c26b1ecae598473456219d9a
smiley <codemasher@users.noreply.github.com>
2022-10-14 18:45:43 +0200

    Extract dependency download into separate action

--- scripts/common.php
+++ scripts/common.php
@@ -81,37 +81,6 @@ function download_file($url){
 }
 
 /**
- * @param string $zipfile
- * @param string $dest
- *
- * @return bool
- */
-function unzip_file($zipfile, $dest){
-	echo "extracting: $zipfile to $dest\n";
-
-	if(!is_readable($zipfile)){
-		echo "zip file not readable: $zipfile\n";
-		return false;
-	}
-
-	$zip = new ZipArchive;
-
-	if($zip->open($zipfile) === false){
-		echo "failed to open zip file: $zipfile\n";
-		return false;
-	}
-
-	if($zip->extractTo($dest) === false){
-		echo "failed to extract zip file: $zipfile to $dest\n";
-		return false;
-	}
-
-	$zip->close();
-
-	return true;
-}
-
-/**
  * @param string $baseurl
  *
  * @return array
